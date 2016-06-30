0ae2770667ccb14e0cacd2010a0b43931a7e3d7a
Wil Chung <iamwil@gmail.com>
2016-06-30 02:04:04 -0700

    can interact with objects

---
+++ Assets/InteractableItem.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractableItem : MonoBehaviour {
+
+	public Rigidbody rb;
+	private bool currentlyInteracting;
+	private HandController attachedHand;
+	private Transform interactionPoint;
+
+	private Vector3 posDelta;
+	private float velocityFactor = 20000f;
+
+	private Quaternion rotationDelta;
+	private float rotationFactor = 400f;
+
+	private float angle;
+	private Vector3 axis;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		interactionPoint = new GameObject().transform;
+
+		currentlyInteracting = false;
+		velocityFactor /= rb.mass;
+		rotationFactor /= rb.mass;
+
+		if (rb == null) {
+			Debug.Log("Interactable Item doesn't have a rigidbody");
+		}
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (attachedHand && IsInteracting()) {
+
+			posDelta = attachedHand.transform.position - interactionPoint.position;
+			this.rb.velocity = posDelta * velocityFactor * Time.fixedDeltaTime;
+
+			rotationDelta = attachedHand.transform.rotation * Quaternion.Inverse(interactionPoint.rotation);
+			rotationDelta.ToAngleAxis(out angle, out axis);
+
+			if (angle > 180) {
+				angle -= 360;
+			}
+
+			rb.angularVelocity = (Time.fixedDeltaTime * angle * axis) * rotationFactor;
+		}
+	}
+
+	public void OnEnterInteraction(HandController hand) {
+		attachedHand = hand;
+
+		interactionPoint.position = hand.transform.position;
+		interactionPoint.rotation = hand.transform.rotation;
+		interactionPoint.SetParent(transform, true);
+
+		currentlyInteracting = true;
+	}
+
+	public void OnExitInteraction(HandController hand) {
+		if (hand == attachedHand) {
+			attachedHand = null;
+			currentlyInteracting = false;
+		}
+	}
+
+	public bool IsInteracting() {
+		return currentlyInteracting;
+	}
+}
---
+++ Assets/Scripts/HandController.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HandController : MonoBehaviour {
+
+	public SteamVR_TrackedObject trackedObj;
+	private SteamVR_Controller.Device controller;
+
+	HashSet<InteractableItem> objectsHoveringOver = new HashSet<InteractableItem>();
+	private InteractableItem closestItem;
+	private InteractableItem interactingItem;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		controller = SteamVR_Controller.Input((int)trackedObj.index);
+
+		if (controller == null) {
+			Debug.Log("Controller not initialized");
+			return;
+		}
+
+		// when using the trigger
+		if (controller.GetPressDown(SteamVR_Controller.ButtonMask.Trigger)) {
+			Debug.Log("trigger and there is object");
+			controller.TriggerHapticPulse(2000);
+
+			float minDistance = float.MaxValue;
+			float distance;
+			foreach (InteractableItem item in objectsHoveringOver) {
+				distance = (item.transform.position - transform.position).sqrMagnitude;
+				if (distance < minDistance) {
+					minDistance = distance;
+					closestItem = item;
+				}
+			}
+			interactingItem = closestItem;
+
+			if (interactingItem) {
+				if (interactingItem.IsInteracting()) {
+					interactingItem.OnExitInteraction(this);
+				}
+				interactingItem.OnEnterInteraction(this);
+			}
+		}
+
+		if (controller.GetPressUp(SteamVR_Controller.ButtonMask.Trigger)) {
+			controller.TriggerHapticPulse(2000);
+
+			if (interactingItem != null) {
+				interactingItem.OnExitInteraction(this);
+			}
+		}
+
+		// when using the grip
+		if (controller.GetPressDown(SteamVR_Controller.ButtonMask.Grip)) {
+			controller.TriggerHapticPulse(2000);
+		}
+
+		if (controller.GetPressUp(SteamVR_Controller.ButtonMask.Grip)) {
+			controller.TriggerHapticPulse(2000);
+		}
+	}
+
+	public Rigidbody getRigidbody() {
+		return GetComponent<Rigidbody>();
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		InteractableItem collidedItem = collider.GetComponent<InteractableItem>();
+		if (collidedItem) {
+			objectsHoveringOver.Add(collidedItem);
+		}
+	}
+
+	void OnTriggerExit(Collider collider) {
+		InteractableItem collidedItem = collider.GetComponent<InteractableItem>();
+		if (collidedItem) {
+			objectsHoveringOver.Remove(collidedItem);
+		}
+	}
+}
