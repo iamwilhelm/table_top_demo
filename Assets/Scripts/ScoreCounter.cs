8c3322714e5d788f1fe83840d82497b17f0f966c
Wil Chung <iamwil@gmail.com>
2016-06-30 17:19:35 -0700

    added trigger for score counter

---
+++ Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreCounter : MonoBehaviour {
+
+	private int score;
+
+	// Use this for initialization
+	void Start () {
+		resetScore();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		Debug.Log("entered hoop");
+	}
+
+	void OnTriggerExit(Collider collider) {
+		scorePoints(1);
+		Debug.Log("Score is now: " + getScore());
+	}
+
+	int getScore() {
+		return score;
+	}
+
+	void scorePoints(int points) {
+		score += points;
+	}
+
+	void resetScore() {
+		score = 0;
+	}
+}
