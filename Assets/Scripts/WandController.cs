6c44c903acdc5762e697924094f656c37362149b
Wil Chung <iamwil@gmail.com>
2016-07-04 20:20:17 -0700

    added fireball creation from wand

---
+++ Assets/Scripts/WandController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WandController : MonoBehaviour {
+
+	public SteamVR_TrackedObject trackedObj;
+	private SteamVR_Controller.Device controller;
+
+	public GameObject fireball;
+	public Transform spellSpawn;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		controller = SteamVR_Controller.Input((int)trackedObj.index);
+
+		if (controller == null) {
+			Debug.Log("Controller not initialized");
+			return;
+		}
+
+		if (controller.GetPressDown(SteamVR_Controller.ButtonMask.Trigger)) {
+			Instantiate(fireball, spellSpawn.position, spellSpawn.rotation);
+		}
+
+	}
+}
