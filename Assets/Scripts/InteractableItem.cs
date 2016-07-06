e2e3a2a1209f742226ef33394a47a454c60fd6f5
Wil Chung <iamwil@gmail.com>
2016-07-05 18:55:37 -0700

    Can pop up shootable items
    Still is buggy

--- Assets/Scripts/InteractableItem.cs
+++ Assets/Scripts/InteractableItem.cs
@@ -69,4 +69,5 @@ public class InteractableItem : MonoBehaviour {
 	public bool IsInteracting() {
 		return currentlyInteracting;
 	}
+
 }
---
+++ Assets/Scripts/ShootableItem.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootableItem : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		this.rb = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnParticleCollision(GameObject particle) {
+		Debug.Log("hitting shootable item");
+		this.rb.AddExplosionForce(10000.0f, particle.transform.position, 1.0f, 10000.0f);
+	}
+
+}
