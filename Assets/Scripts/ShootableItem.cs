7c79386eff8aa6b46516b806ab72f556a749d6f6
Wil Chung <iamwil@gmail.com>
2016-07-06 17:05:58 -0700

    Can apply splash forces on dynamic objects in world

--- Assets/Scripts/ShootableItem.cs
+++ Assets/Scripts/ShootableItem.cs
@@ -7,16 +7,15 @@ public class ShootableItem : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		this.rb = GetComponent<Rigidbody>();
+		rb = GetComponent<Rigidbody>();
 	}
 
 	// Update is called once per frame
 	void Update () {
 	}
 
-	void OnParticleCollision(GameObject particle) {
-		Debug.Log("hitting shootable item");
-		this.rb.AddExplosionForce(10000.0f, particle.transform.position, 1.0f, 10000.0f);
+	void OnParticleCollision(GameObject projectile) {
+		Debug.Log(projectile.transform.position);
 	}
 
 }
---
+++ Assets/Scripts/SplashDamage.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class SplashDamage : MonoBehaviour {
+
+	public float radius;
+	public float force;
+
+	private ParticleSystem pSystem;
+	private ParticleSystem.Particle[] particles;
+
+	// Use this for initialization
+	void Start () {
+		if (pSystem == null)
+			pSystem = GetComponent<ParticleSystem>();
+
+		if (particles == null || particles.Length < pSystem.maxParticles)
+			particles = new ParticleSystem.Particle[pSystem.maxParticles];
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnParticleCollision(GameObject hitObject) {
+		RaycastHit hitInfo;
+		Physics.Raycast(transform.position, transform.forward, out hitInfo);
+
+		Collider[] colliders = Physics.OverlapSphere(hitInfo.point, this.radius);
+
+		foreach (Collider c in colliders) {
+			// skip all colliders without a rigidbody, so we don't explode static elements
+			if (c.GetComponent<Rigidbody>() == null) continue;
+			Debug.Log(c);
+
+			// push all within the splash damage radius out
+			c.GetComponent<Rigidbody>().AddExplosionForce(force, hitInfo.point, radius, 0.5f, ForceMode.Impulse);
+		}
+	}
+
+}
