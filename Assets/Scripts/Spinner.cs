0aa805062b641655eedae1985a39e8623a539821
Wil Chung <iamwil@gmail.com>
2016-07-15 17:34:31 -0700

    add spinning coin

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spinner : MonoBehaviour {
+
+	public float angularVelocity;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(new Vector3(0, angularVelocity, 0) * Time.deltaTime);
+	}
+}
