15c37bef9d39894def3e7098c7386580da56d99d
Wil Chung <iamwil@gmail.com>
2016-07-16 21:09:26 -0700

    add second level

--- Assets/Scripts/Teleportation.cs
+++ Assets/Scripts/Teleportation.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class Teleportation : MonoBehaviour {
 
 	public ParticleSystem teleportIndicator;
+	public Transform teleportTarget;
+
+	private float teleportTime = -1;
 
 	// Use this for initialization
 	void Start () {
@@ -11,13 +14,23 @@ public class Teleportation : MonoBehaviour {
 	}
 
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+		if (teleportTime == -1) return;
+		if (Time.fixedTime < teleportTime + 1) return;
+
+		GameObject cameraRig = GameObject.Find("/[CameraRig]");
+		cameraRig.transform.position = teleportTarget.position;
+		cameraRig.transform.rotation = teleportTarget.rotation;
+
+		teleportTime = -1;
 	}
 
 	void OnTriggerEnter(Collider collider) {
 		Debug.Log("enter transporter");
 		if (collider.gameObject.name != "Head") return;
 		teleportIndicator.Play();
+
+		teleportTime = Time.fixedTime;
 	}
 
 	void OnTriggerExit() {
