94a4c15ebee68d8e11f430fb67f0441d2d0f6499
Wil Chung <iamwil@gmail.com>
2016-07-16 17:50:30 -0700

    added button

---
+++ Assets/Scripts/ButtonTrigger.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonTrigger : MonoBehaviour {
+
+	public float max = 0.16f;
+	public float min = 0.11f;
+
+	private bool buttonOn = false;
+	private Transform buttonSurfaceTf;
+
+	// Use this for initialization
+	void Start () {
+		buttonSurfaceTf = transform.Find("Surface");
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (buttonOn == true) {
+			buttonSurfaceTf.position = new Vector3(0, min, 0);
+		} else {
+			buttonSurfaceTf.position = new Vector3(0, max, 0);
+		}
+	}
+
+	void OnTriggerEnter() {
+		Debug.Log("enter");
+		buttonOn = true;
+	}
+
+	void OnTriggerExit() {
+		Debug.Log("exit");
+		buttonOn = false;
+	}
+}
