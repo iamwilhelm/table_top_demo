44a6937cf18b522a2e9cb0b063a53ce6b2b47253
Wil Chung <iamwil@gmail.com>
2016-07-16 19:29:29 -0700

    Can trigger a teleporter and its particle system

---
+++ Assets/Scripts/Teleportation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleportation : MonoBehaviour {
+
+	public ParticleSystem teleportIndicator;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		Debug.Log("enter transporter");
+		if (collider.gameObject.name != "Head") return;
+		teleportIndicator.Play();
+	}
+
+	void OnTriggerExit() {
+	}
+
+}
